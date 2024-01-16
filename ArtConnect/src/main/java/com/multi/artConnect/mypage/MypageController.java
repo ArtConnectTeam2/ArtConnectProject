1b36dac61042a4447b37d3d8454227995828c705
Kimsoyeon <sykim557@naver.com>
2024-01-16 16:19:15 +0900

    review, reservation - jsp, mapper 파일 생성 좋아요 - Controller, mapper, dao 추가 - 내 리뷰 페이지 조회, 페이징 추가 - 내 예약 페이지 추가

--- ArtConnect/src/main/java/com/multi/artConnect/mypage/MypageController.java
+++ ArtConnect/src/main/java/com/multi/artConnect/mypage/MypageController.java
@@ -1,6 +1,10 @@
 package com.multi.artConnect.mypage;
 
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.multi.artConnect.reservation.ReservationVO;
+import com.multi.artConnect.review.BoardVO;
+import com.multi.artConnect.review.PageVO;
+import java.util.List;
+import java.util.Map;
 
 
 @Controller
@@ -57,7 +66,7 @@ public class MypageController {
 
 	    // 비밀번호 불일치 - 탈퇴 실패
 	    redirectAttributes.addFlashAttribute("result", "deleteNo");
-	    return "mypage/deleteOk";
+	    return "mypage/memberDelete";
 	}
 	
 	 @GetMapping("mypage/deleteOk") 
@@ -70,4 +79,46 @@ public class MypageController {
 	     return "mypage/deleteNo";
 	 }
 
+	 
+	 @GetMapping("mypage/myReview")
+	 public String myReviewList(Model model, HttpSession session,
+	                            @RequestParam(name = "page", defaultValue = "1") int page,
+	                            @RequestParam(name = "size", defaultValue = "5") int size) {
+	     String memberID = (String) session.getAttribute("memberID");
+	     int totalCount = dao.getTotalCount(memberID); 
+
+	     PageVO pageVO = new PageVO(page, size, totalCount);
+	     int start = pageVO.getStart(); // 페이징 시작 
+	     int end = pageVO.getEnd();     // 페이징 종료 
+
+	     List<BoardVO> review = dao.myReviewList(memberID, start, size); 
+	     model.addAttribute("list", review);    
+	     model.addAttribute("pageVO", pageVO);   
+
+	     return "mypage/myReview"; 
+	 }
+	 
+	  @GetMapping("mypage/myReservation")
+	    public String myReservationList(Model model, HttpSession session) {
+	        String memberID = (String) session.getAttribute("memberID");
+	        List<ReservationVO> reservation = dao.myReservationList(memberID);
+	        model.addAttribute("list", reservation);
+	        return "mypage/myReservation";
+	    }
+	 
+	// MemberID에 따른 Like 목록 가져오기
+		@RequestMapping("mypage/myLike")
+		public String myLike(HttpSession session, Model model) {
+			  // 현재 로그인한 회원의 아이디 가져오기 (세션에서)
+		    String memberID = (String) session.getAttribute("memberID");
+
+		    // 좋아요 정보를 가져오는 DAO 메소드 호출
+		    List<Map<String, Object>> likesList = dao.myLike(memberID);
+
+		    // Model에 좋아요 정보를 추가
+		    model.addAttribute("likesList", likesList);
+
+		    // 마이페이지로 이동
+		    return "mypage/myLike";
+		}
 }
