9e5db77d22a351df1507a3a0c84f647008c7f7fb
seoyoung <mymay99@naver.com>
2024-01-16 16:18:02 +0900

    <게시판> 첨부파일 추가 <댓글> 댓글 작성 및 댓글 조회 페이지 추가 진행중

--- ArtConnect/src/main/java/com/multi/artConnect/review/ReplyController.java
+++ ArtConnect/src/main/java/com/multi/artConnect/review/ReplyController.java
@@ -1,27 +1,29 @@
-/*
- * package com.multi.artConnect.review;
- * 
- * import org.springframework.beans.factory.annotation.Autowired; import
- * org.springframework.web.bind.annotation.*;
- * 
- * import java.util.List;
- * 
- * @RestController
- * 
- * @RequestMapping("/api/replies") public class ReplyController {
- * 
- * @Autowired private ReplyDAO replyDAO;
- * 
- * // 특정 리뷰에 대한 모든 댓글 가져오기
- * 
- * @GetMapping("/review/{reviewNO}") public List<ReplyVO>
- * getRepliesByReview(@PathVariable int reviewNO) { return
- * replyDAO.getRepliesByReview(reviewNO); }
- * 
- * // 새로운 댓글 추가하기
- * 
- * @PostMapping("/add") public ReplyVO addReply(@RequestBody ReplyVO replyVO) {
- * return replyDAO.addReply(replyVO); }
- * 
- * // 업데이트, 삭제 등을 위한 다른 필요한 엔드포인트도 추가 가능합니다. }
- */
+package com.multi.artConnect.review;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/review")
+public class ReplyController {
+
+    @Autowired
+    private ReplyDAO replyDAO;
+
+    //댓글 추가 처리
+    @RequestMapping(value = "insertReply", method = RequestMethod.POST)
+    public String insertReply(ReplyVO reply, Model model) {
+    	
+    	String memberID = "임시ID";
+		// 댓글에 사용자의 ID 설정
+        reply.setMemberID(memberID);
+    	//댓글 추가 
+    	replyDAO.insertReply(reply);
+    	
+        // 댓글 추가 후 게시글 상세 페이지로 리다이렉트
+        return "redirect:boardOne?reviewNO=" + reply.getReviewNO();
+    }
+}
